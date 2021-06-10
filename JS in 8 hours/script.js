1b6557d11158d57a1c285043c7611f855a40f7c6
nateriver2204 <73120991+nateriver2204@users.noreply.github.com>
2021-06-10 19:48:37 +0700

    okok

--- JS in 8 hours/script.js
+++ JS in 8 hours/script.js
@@ -28,19 +28,18 @@ function generateCat() {
 
 // Challeng 3: Rock, Paper, Scissors
 function rpsGame(yourChoice) {
-  console.log(yourChoice);
   let humanChoice, botChoice;
   humanChoice = yourChoice.id;
-  console.log(humanChoice);
+
   //random bot choice
   botChoice = numberToChoice(randomRps());
-  console.log(botChoice);
+
   //win function
   results = decideWinner(humanChoice, botChoice);
-  console.log(results);
+
   //message print
   message = finalMessage(results);
-  console.log(message);
+
   //fronEnd
   rpsFrontEnd(humanChoice, botChoice, message);
 }
@@ -91,8 +90,6 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
   var humanDiv = document.createElement("div");
   var botDiv = document.createElement("div");
   var messageDiv = document.createElement("div");
-
-  console.log(humanImageChoice);
 
   humanDiv.innerHTML =
     "<img src='" +
@@ -163,4 +160,82 @@ function randomColor() {
     all_buttons[i].classList.remove(all_buttons[i].classList[1]);
     all_buttons[i].classList.add(choices[rand]);
   }
+}
+
+//Challenge 5: Blackjack
+let blackjackGame = {
+  you: {
+    scoreSpan: "#your-blackjack-result",
+    div: "#yourbox",
+    score: 0,
+  },
+  dealer: {
+    scoreSpan: "#dealer-blackjack-result",
+    div: "#dealerbox",
+    score: 0,
+  },
+  cards: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+  cardsMap: {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 10,
+    Q: 10,
+    K: 10,
+    A: [1, 11],
+  },
+};
+const YOU = blackjackGame["you"];
+const DEALER = blackjackGame["dealer"];
+const hitSound = new Audio("/sounds/swish.m4a");
+const CARDS = blackjackGame["cards"];
+
+document.querySelector("#bj-hit-btn").addEventListener("click", blackjackHit);
+
+document.querySelector("#bj-deal-btn").addEventListener("click", blackjackDeal);
+
+function blackjackHit() {
+  //hit random card
+  let card = randomCard();
+  console.log(card);
+  //show cards
+  showCard(card, YOU);
+  //set score
+  updateScore(card, YOU);
+  //switch player when click stand
+}
+function randomCard() {
+  let randomIndex = Math.floor(Math.random() * 13);
+  return CARDS[randomIndex];
+}
+function updateScore(card, activePlayer) {
+  activePlayer["score"] += blackjackGame["cardsMap"][card];
+  document.querySelector(activePlayer.scoreSpan).innerText =
+    activePlayer["score"];
+}
+
+function showCard(card, activePlayer) {
+  let cardImage = document.createElement("img");
+  cardImage.src = `/images/${card}.png`;
+  document.querySelector(activePlayer.div).appendChild(cardImage);
+  hitSound.play();
+}
+
+function blackjackDeal() {
+  let yourImages = document.querySelector(YOU.div).querySelectorAll("img");
+  let dealerImages = document.querySelector(DEALER.div).querySelectorAll("img");
+  //remove img
+  for (const image of yourImages) {
+    image.remove();
+  }
+  for (const image of dealerImages) {
+    image.remove();
+  }
+  //reset score
 }
